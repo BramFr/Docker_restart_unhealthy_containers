1e2bc096ba71d569b09eb3b2ee5ba227730948a5
Bram Fransen <bfransen@insign.it>
2018-11-21 06:45:14 +0100

    syntax aangepast

--- dockerUnhealthyRestart.py
+++ dockerUnhealthyRestart.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import docker
-import os
-import json
-import time
 from slackclient import SlackClient
+import time
+import json
+import os
+import docker
+
 
 
 client = docker.APIClient(base_url='unix://var/run/docker.sock')
@@ -13,35 +14,36 @@ sc = SlackClient(slack_token)
 
 
 def main():
-  if queryFileLocation():
-    while True:
-      queryStatusContainer()
-      time.sleep(60)
+    if queryFileLocation():
+        while True:
+            queryStatusContainer()
+            time.sleep(60)
 
 def queryFileLocation():
-  if os.path.exists(docker_file_location):
-    return os.path.exists(docker_file_location)
-  else:
-    print("Docker is not installed. Please install first!")
-    exit()
+    if os.path.exists(docker_file_location):
+        return os.path.exists(docker_file_location)
+    else:
+        print("Docker is not installed. Please install first!")
+        exit()
 
 def queryStatusContainer():
     for container in client.containers():
-      healthyStatus = None
-      try:
-        healthyStatus = client.inspect_container(container['Id'])['State']['Health']['Status']
-      finally:
-        if healthyStatus != "healthy":
-          client.restart(container['Id'])
-          if slack_token != "SLACK_API_TOKEN":
-            sc.api_call(
-              "chat.postMessage",
-              channel="zabbix_monitor",
-              text="Container {} was unhealthy.".format(container['Names']),
-              user="PythonScript"
-              )
- 
-    
+        healthyStatus = None
+        try:
+            healthyStatus = client.inspect_container(
+                container['Id'])['State']['Health']['Status']
+        finally:
+            if healthyStatus != "healthy":
+                client.restart(container['Id'])
+                if slack_token != "SLACK_API_TOKEN":
+                    sc.api_call(
+                        "chat.postMessage",
+                        channel="zabbix_monitor",
+                        text="Container {} was unhealthy.".format(
+                            container['Names']),
+                        user="PythonScript"
+                    )
+
+
 if __name__ == '__main__':
     main()
-    
