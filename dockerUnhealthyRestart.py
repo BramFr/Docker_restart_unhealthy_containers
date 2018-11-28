c3304fa05d104c773723649bbcc0d5dbe0897a12
Bram Fransen <bfransen@insign.it>
2018-11-28 14:02:55 +0100

    Fix script healthy check.

--- dockerUnhealthyRestart.py
+++ dockerUnhealthyRestart.py
@@ -10,7 +10,9 @@ import docker
 client = docker.APIClient(base_url='unix://var/run/docker.sock')
 docker_file_location = '/usr/bin/docker'
 slack_token = "SLACK_API_TOKEN"
+slack_channel = "ChangeMe"
 sc = SlackClient(slack_token)
+
 
 
 def main():
@@ -30,15 +32,18 @@ def queryStatusContainer():
     for container in client.containers():
         healthyStatus = None
         try:
+            print(container['Id'])
             healthyStatus = client.inspect_container(
                 container['Id'])['State']['Health']['Status']
+        except:
+            pass
         finally:
-            if healthyStatus != "healthy":
+            if healthyStatus == "unhealthy":
                 client.restart(container['Id'])
                 if slack_token != "SLACK_API_TOKEN":
                     sc.api_call(
                         "chat.postMessage",
-                        channel="zabbix_monitor",
+                        channel="{}".format(slack_channel),
                         text="Container {} was unhealthy.".format(
                             container['Names']),
                         user="PythonScript"
